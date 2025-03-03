97eab8dbe76fb50326ac560533cbd750bc86e3eb
Tejasvi Sharma <134033214+iemtejasvi@users.noreply.github.com>
2025-03-04 01:02:55 +0530

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../lib/supabaseClient";
 import MemoryCard from "../components/MemoryCard";
 
 interface Memory {
