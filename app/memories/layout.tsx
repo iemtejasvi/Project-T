0663bd5d5c702a88dd889aa8ef6e726d946d96e3
Tejasvi Sharma <stejasvi817@gmail.com>
2025-06-17 00:25:27 +0530

    Fixes

--- app/memories/layout.tsx
+++ app/memories/layout.tsx
@@ -20,5 +20,20 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
   },
-}; 
+};
+
+export default function MemoriesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+} 
