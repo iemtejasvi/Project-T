aa15e7b36ab5d0abd20cd5d0d76b373c33b7c0d6
iemtejasvi <stejasvi817@gmail.com>
2025-07-29 02:09:34 +0530

    Fixes

--- components/BleedingText.tsx
+++ components/BleedingText.tsx
@@ -7,22 +7,22 @@ interface BleedingTextProps {
 
 const BleedingText: React.FC<BleedingTextProps> = ({ message, textClass }) => {
   return (
-    <div className="bleeding-text-container">
-      <svg>
+    <div className="bleeding-text pl-2 pr-[0.125rem] antialiased space-y-2 overflow-hidden">
+      <svg style={{ position: 'absolute', top: '-999em' }}>
         <filter id="cracked">
-          <feGaussianBlur stdDeviation=".5" in="SourceGraphic" result="B" />
-          <feTurbulence result="T" numOctaves="7" seed="488" baseFrequency=".042" />
+          <feGaussianBlur stdDeviation="0.5" in="SourceGraphic" result="B" />
+          <feTurbulence result="T" numOctaves="7" seed="488" baseFrequency="0.042" />
           <feDisplacementMap in2="T" xChannelSelector="R" yChannelSelector="G" in="T" result="D" />
           <feComposite in2="D" operator="in" in="B" result="C" />
           <feSpecularLighting in="C" specularExponent="65" specularConstant="2" result="S" surfaceScale="2">
             <feDistantLight azimuth="225" elevation="62" />
           </feSpecularLighting>
-          <feComposite in2="C" operator="arithmetic" k1="2.5" k2="-.5" k3="1" in="S" result="C" />
+          <feComposite in2="C" operator="arithmetic" k1="2.5" k2="-0.5" k3="1" in="S" result="C" />
           <feBlend in2="C" mode="multiply" result="A" />
-          <feColorMatrix result="fC" in="A" values="1 0 0 -1 0 1 0 1 -1 0 1 0 0 -1 0 -2 -0.5 0 5 -2"/>
+          <feColorMatrix result="fC" in="A" values="1 0 0 -1 0 1 0 1 -1 0 1 0 0 -1 0 -2 -0.5 0 5 -2" />
           <feGaussianBlur result="B" in="fC" stdDeviation="8" />
           <feOffset in="B" dy="2" dx="2" />
-          <feSpecularLighting specularExponent="15" specularConstant=".8" surfaceScale="4" result="S" in="B">
+          <feSpecularLighting specularExponent="15" specularConstant="0.8" surfaceScale="4" result="S" in="B">
             <fePointLight z="20000" y="-10000" x="-5000" />
           </feSpecularLighting>
           <feComposite in2="fC" operator="in" in="S" result="C" />
@@ -30,9 +30,19 @@ const BleedingText: React.FC<BleedingTextProps> = ({ message, textClass }) => {
           <feBlend in2="C" mode="darken" />
         </filter>
       </svg>
-      <div className="bleeding-text pl-2 pr-[0.125rem] antialiased space-y-2">
-        <p className={textClass}>{message}</p>
-      </div>
+      <p 
+        className={`${textClass} bleeding-cracked`}
+        style={{ 
+          color: 'red',
+          textShadow: '0 0 0.7px red, 0 0 1.1px red, 0 0 1.1px red',
+          filter: 'url(#cracked)',
+          overflowWrap: 'break-word',
+          wordWrap: 'break-word',
+          maxWidth: '100%'
+        }}
+      >
+        {message}
+      </p>
     </div>
   );
 };
