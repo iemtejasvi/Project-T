fe4114534cf84dd541e20707003d931e830deacb
iemtejasvi <stejasvi817@gmail.com>
2025-09-02 00:00:32 +0530

    Fixes

--- app/memories/page.tsx
+++ app/memories/page.tsx
@@ -15,10 +15,12 @@ interface Memory {
   status: string;
   color: string;
   full_bg: boolean;
-  letter_style: string;
   animation?: string;
   pinned?: boolean;
   pinned_until?: string;
+  ip?: string;
+  country?: string;
+  uuid?: string;
   tag?: string;
   sub_tag?: string;
 }
